54a17942737db2cad5b6c1120dea6d4b5f498409
Tolu Joel <toluisjoel@gmail.com>
2024-05-26 23:55:56 +0100

    feat: Add Flask app with endpoint to trigger blog data updates and send email notifications

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+
+from blog import add_new_blogs, update_blog_data
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/new', methods=['GET'])
+def get_new_aricles():
+    json_file_path = 'blogs.json'
+
+    add_new_blogs(json_file_path)
+    articles = update_blog_data(json_file_path)
+
+    return jsonify(articles)
+
+
+if __name__ == '__main__':
+    app.run()
